68c274ed824d52b47bd0af3f4c4a7860fd34393c
LukaFilipovic471 <81623295+LukaFilipovic471@users.noreply.github.com>
2024-09-25 17:09:11 +0200

    Add files via upload

---
+++ Tests/CSP.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int x = 6, y = 9, z = 14, a = 17;
+    int exp = x * y * z + x * a * z;
+    printf("%d\n", exp);
+    
+    return 0;
+}
